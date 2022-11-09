6f826258b460855a3a2554f9de70acfd35cc0fe9
Ben Menking <bmenking@wng.org>
2022-11-08 21:56:30 -0500

    added static function getApi that uses Environment settings

--- src/Advantage.php
+++ src/Advantage.php
@@ -5,6 +5,33 @@ namespace Advantage;
 require('Shim.php');
 
 class Advantage {
+    public static function getApi(String $api) {
+        $env = Environment::currentEnvironment();
+
+        $config = new \Advantage\Client\Configuration();
+
+        $config->setHost($env->getEndPoint());
+        $config->setUsername($env->getUsername());
+        $config->setPassword($env->getPassword());
+
+        $class = "\\Advantage\\Client\\Api\\{$api}";
+        
+        $instance = new $class(
+            new \GuzzleHttp\Client(
+                [
+                    'base_uri'=>$env->getEndPoint(),
+                    'verify'=>true, 
+                    'auth'=>[$env->getUsername(), $env->getPassword()],
+                    'connect_timeout'=>15,
+                    'debug'=>false
+                ]
+            ),
+            $config
+        );
+
+        return $instance;
+    }
+    
     public static function getApiService(String $endpoint, String $api, array $authentication = [], $timeout = 15, $debug = false, $verify_host_ssl = true) {
 
         $config = new \Advantage\Client\Configuration();
